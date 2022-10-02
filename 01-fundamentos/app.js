e412fa8e28972ad8fde9f82d4fd2b20d83362a30
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-01 22:59:38 -0500

    Debug f5

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -26,19 +26,57 @@
 // console.error(x);
 // console.info(x);
 
+// More console types:
+// console.assert()
+// console.clear()
+// console.context()
+// console.count()
+// console.countReset()
+// console.createTask()
+// console.debug()
+// console.dir()
+// console.dirxml()
+// console.group()
+// console.groupCollapsed()
+// console.groupEnd()
+// console.memory() // MemoryInfo: {totalJSHeapSize: 10000000, usedJSHeapSize: 10000000, jsHeapSizeLimit: 2190000000}
+// console.profile()
+// console.profileEnd()
+// console.table()
+// console.time()
+// console.timeEnd()
+// console.timeLog()
+// console.timeStamp()
+// console.trace()
+
 // =================================== //
 
-let a = 10,
-  b = 20,
-  c = 30;
+// let a = 10,
+//   b = 20,
+//   c = 30;
 
 // The console.log method receives all the arguments that we want separated by commas (,)
-console.log(`a:`, a, 'b:', b, 'c:', c);
+// console.log(`a:`, a, 'b:', b, 'c:', c);
 
 // print as object
-console.log({ a });
-console.log({ b });
-console.log({ c });
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
 
 // CSS in the console.log:
-console.log('%c Colors CSS', 'color:blue; font-weight: bold');
+// console.log('%c Colors CSS', 'color:blue; font-weight: bold');
+
+// =================================== //
+
+// Table:
+let a = 'Hello',
+  b = 'World',
+  c = 30;
+console.table({ a, b, c });
+
+// document
+// document.write('Hello Elis');
+// console.info(document.baseURI);
+
+// F5 Run and Debug
+// Open Debug Console: Ctrl+Shift+Y
