c5537d0a35a3cb13c2941d29d69e2eb1d96303b3
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-01 20:54:06 -0500

    uncaught typeError: Assignment to variable at app.js:9:3

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,1 +1,9 @@
-console.log(console.log('Hello world')); // Hello world - undefined
+// console.log(console.log('Hello world')); // Hello world - undefined
+
+// Variable
+
+let a = 10;
+var b = 10;
+const c = 10;
+
+// c = 20; // Uncaught TypeError: Assignment to constant variable at app.js:9:3
