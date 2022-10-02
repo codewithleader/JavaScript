4968a19afc24752d7e3c60ce9976dc247fddbfde
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-01 21:10:56 -0500

    Another way to declare variables

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -2,9 +2,19 @@
 
 // Variable
 
-let a = 10;
-var b = 10;
-const c = 10;
-d = 10; // Bad practice
+// let a = 10;
+// var b = 10;
+// const c = 10;
+// d = 10; // Bad practice
 
 // c = 20; // Uncaught TypeError: Assignment to constant variable at app.js:9:3
+
+// Another way to declare variables:
+
+let a = 10,
+  b = 20,
+  c = 3,
+  d = 5,
+  x = a + b;
+
+console.log(x);
