9720b2e42f5ec6861349f67cdeb990676e39fe5e
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-01 22:05:21 -0500

    method console.log

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -9,17 +9,33 @@
 
 // c = 20; // Uncaught TypeError: Assignment to constant variable at app.js:9:3
 
+// =================================== //
+
 // Another way to declare variables:
 
-let a = 10,
-  b = 20,
-  c = 3,
-  d = 5,
-  x = a + b;
+// let a = 10,
+//   b = 20,
+//   c = 3,
+//   d = 5,
+//   x = a + b;
 
 // Console types:
 
-console.log(x);
-console.warn(x);
-console.error(x);
-console.info(x);
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+// console.info(x);
+
+// =================================== //
+
+let a = 10,
+  b = 20,
+  c = 30;
+
+// The console.log method receives all the arguments that we want separated by commas (,)
+console.log(`a:`, a, 'b:', b, 'c:', c);
+
+// print as object
+console.log({ a });
+console.log({ b });
+console.log({ c });
